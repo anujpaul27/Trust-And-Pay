b8a7916d9f1ff60c6dcae1674e6412cc3d797f43
anuj paul <paulanuj27@gmail.com>
2025-11-01 20:27:42 +0600

    validate login information

--- JavaScript/home.js
+++ JavaScript/home.js
@@ -1,7 +1,24 @@
+// Login validation 
+let number = document.getElementById('number').value
+let pin = document.getElementById('pin').value 
+document.getElementById('login-btn').addEventListener('click',function(){
+    
+    if (number === '01646267167' && pin === '1144')
+    {
+        window.location.href = 'home.html'
+    }
+    else 
+    {
+        alert('Something is wrong!. try again')
+    }
+})
+
+
 // Show Balance 
 balance = 1200
 
 const val= document.getElementById('balance')
 val.addEventListener('click',function(){
    val.innerText = balance
+   console.log("Hello Bs")
 })
